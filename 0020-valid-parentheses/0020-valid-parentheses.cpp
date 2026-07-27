0e3a172b6e459914ba0b254b26c53947570ca874
Nimit Jain <jainnimit719@gmail.com>
2026-07-28 00:43:50 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.43%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            }
+            if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
+                if (st.empty()) {
+                    return false;}
+
+                    char top = st.top();
+                
+                  if ((top=='(' && s[i]==')')|| (top=='{' && s[i]=='}')||(top=='[' && s[i]==']')   ) {
+                    st.pop();
+                } 
+                else {
+                    return false;
+                }
+            }
+        }
+        if(st.empty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
