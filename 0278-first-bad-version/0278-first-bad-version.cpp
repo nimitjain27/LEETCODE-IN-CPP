5fe9f832530e62dd1b192bbac37196ed002a4cad
Nimit Jain <jainnimit719@gmail.com>
2026-08-01 21:24:35 +0530

    Time: 2944 ms (1.86%), Space: 8 MB (42.21%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -6,28 +6,24 @@ public:
     int firstBadVersion(int n) {
         int left = 1;
         long long right = n;
-        int ans=0;
+        int ans = 0;
 
-        long long mid=(left+right-1)/2;
+        long long mid = left+(right-left)/2;
 
-        while(left <= right){
+        while (left <= right) {
 
-               if(isBadVersion(mid)){
-                    ans=mid;
-                    if(isBadVersion(mid-1)){
-                        mid =mid-1;
-                    }
-                    else{
-                        return ans;
-                    }
-                   
-               }
-               else {
-                    mid=mid+1;
-               }
+            if (isBadVersion(mid)) {
+                ans = mid;
+                if (isBadVersion(mid - 1)) {
+                    mid = mid - 1;
+                } else {
+                    return ans;
+                }
 
+            } else {
+                mid = mid + 1;
+            }
         }
-
         return ans;
     }
 };
