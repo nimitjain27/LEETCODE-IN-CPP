e7e05f372a8b973686ed73ce8ad92182c49586d4
Nimit Jain <jainnimit719@gmail.com>
2026-08-01 17:08:43 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.63%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        long long low = 1;
+        long long high = x;
+        int ans = 0;
+
+        while (low <= high) {
+
+            long long mid = (low + high) / 2;
+
+            if (mid * mid == x) {
+                return mid;
+            }
+
+            if (mid * mid < x) {
+                ans = mid;
+                low = mid + 1;
+            }
+             else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
